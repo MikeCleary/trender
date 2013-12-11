b63e220b5844b0eccfd1aedd8004109b11eef858
Michael <Cleary481@gmail.com>
2013-12-11 18:02:26 +0000

    added transaction block to story class

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,14 +2,17 @@ class Story < ActiveRecord::Base
   belongs_to :trend
 
   def self.parse_feedzilla(json, trend_id)
+    
     json['articles'].each do |article|
-      Story.create(
-        :title => article['title'],
-        :url => article['url'],
-        :source => article['source'],
-        :published => article['publish_date'],
-        :trend_id => trend_id
-      )
+      ActiveRecord::Base.transaction do
+        Story.new(
+          :title => article['title'],
+          :url => article['url'],
+          :source => article['source'],
+          :published => article['publish_date'],
+          :trend_id => trend_id
+        )
+      end
     end
   end
 
