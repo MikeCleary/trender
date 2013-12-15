cb8b2dd3dfaf69769faf7e26d02f0fe3666fe8db
Michael <Cleary481@gmail.com>
2013-12-15 13:05:38 +0000

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213154136) do
+ActiveRecord::Schema.define(version: 20131215130410) do
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.string   "country"
