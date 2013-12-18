572a3e44f9f5b163a4179c7c79bd1375f4dad477
Michael <Cleary481@gmail.com>
2013-12-18 10:29:05 +0000

    rejigged keys properly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 
 if Rails.env == "production"
-  provider :twitter, ENV['twitter_id'], ENV['twitter_secret'] 
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, ENV['twitter_id'], ENV['twitter_secret'] 
+  end
 else
   twitter_secrets = YAML::load_file('config/omniauth.yml')[Rails.env]
   Rails.application.config.middleware.use OmniAuth::Builder do
